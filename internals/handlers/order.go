5ed917b20fec0f9dfc053c9287e2972528521236
markmumba <markmumba01@gmail.com>
2023-11-24 12:15:59 +0300

    implemented the order create function

--- internals/handlers/order.go
+++ internals/handlers/order.go
@@ -1,1 +1,47 @@
 package handlers
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/markmumba/fiber-api/database"
+	"github.com/markmumba/fiber-api/internals/models"
+)
+
+type OrderSerializer struct {
+	ID        uint              `json:"id"`
+	User      UserSerializer    `json:"user"`
+	Product   ProductSerializer `json:"product"`
+	CreatedAt time.Time
+}
+
+func CreateSerialOrder(order models.Order, user UserSerializer, product ProductSerializer) OrderSerializer {
+	return OrderSerializer{ID: order.ID, User: user, Product: product, CreatedAt: order.CreatedAt}
+
+}
+
+func CreateOrder(c *fiber.Ctx) error {
+
+	var order models.Order
+	if err := c.BodyParser(&order); err != nil {
+		c.Status(500).JSON(err.Error())
+	}
+
+	var user models.User
+	if err := FindUser(order.UserRefer, &user); err != nil {
+		c.Status(400).JSON(err.Error())
+	}
+
+	var product models.Product
+	if err := FindProduct(order.ProductRefer, &product); err != nil {
+		c.Status(400).JSON(err.Error())
+	}
+	database.Database.DB.Create(&order)
+
+	responseUser := CreateSerialUser(user)
+	responseProduct := CreateSerialProduct(product)
+	responseOrder := CreateSerialOrder(order, responseUser, responseProduct)
+
+	return c.Status(200).JSON(responseOrder)
+
+}
